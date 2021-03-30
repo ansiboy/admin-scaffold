4d3cfa4952d259845378dc161679adc53bd94d86
mai.shu <ansiboy@163.com>
2021-03-30 16:20:16 +0800

    append dilu

--- static/pre-required.ts
+++ static/pre-required.ts
@@ -5,6 +5,8 @@ exports["maishu-chitu-react"] = require("maishu-chitu-react");
 exports["maishu-chitu-service"] = require("maishu-chitu-service");
 exports["maishu-toolkit"] = require("maishu-toolkit");
 exports["maishu-ui-toolkit"] = require("maishu-ui-toolkit");
+exports["maishu-dilu"] = require("maishu-dilu");
+exports["maishu-dilu-react"] = require("maishu-dilu-react");
 exports["css"] = require("maishu-requirejs-plugins/src/css");
 exports["text"] = require("maishu-requirejs-plugins/lib/text");
 
