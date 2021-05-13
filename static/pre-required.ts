f0ef0a75feaa49a34a89c368253193c08d8c2f40
mai.shu <ansiboy@163.com>
2021-05-13 09:06:50 +0800

    update

--- static/pre-required.ts
+++ static/pre-required.ts
@@ -1,12 +1,12 @@
-exports["react"] = require("react");
-exports["react-dom"] = require("react-dom");
+// exports["react"] = require("react");
+// exports["react-dom"] = require("react-dom");
 exports["maishu-chitu"] = require("maishu-chitu");
 exports["maishu-chitu-react"] = require("maishu-chitu-react");
 exports["maishu-chitu-service"] = require("maishu-chitu-service");
-exports["maishu-data-page"] = require("maishu-data-page");
+// exports["maishu-data-page"] = require("maishu-data-page");
 exports["maishu-toolkit"] = require("maishu-toolkit");
 exports["maishu-ui-toolkit"] = require("maishu-ui-toolkit");
-exports["maishu-dilu"] = require("maishu-dilu");
+// exports["maishu-dilu"] = require("maishu-dilu");
 exports["maishu-dilu-react"] = require("maishu-dilu-react");
 exports["css"] = require("maishu-requirejs-plugins/src/css");
 exports["text"] = require("maishu-requirejs-plugins/lib/text");
